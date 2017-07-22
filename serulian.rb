605cd2b88349d6f06db312d5817e7deeec93cb60
josephschorr <josephschorr@users.noreply.github.com>
2017-07-22 19:46:00 -0400

    Adjust for GitHub organization name change

--- serulian.rb
+++ serulian.rb
@@ -3,20 +3,20 @@ require "language/go"
 class Serulian < Formula
   desc "Serulian compiler and toolkit"
   homepage "http://serulian.io"
-  url "https://github.com/Serulian/compiler.git"
-  head "https://github.com/Serulian/compiler.git"
+  url "https://github.com/serulian/compiler.git"
+  head "https://github.com/serulian/compiler.git"
   version "alpha"
 
   depends_on "go" => :build
 
   def install
     mkdir_p buildpath/"src/github.com/Serulian"
-    ln_s buildpath, buildpath/"src/github.com/Serulian/compiler"
+    ln_s buildpath, buildpath/"src/github.com/serulian/compiler"
 
     ENV["GOPATH"] = buildpath
 
-    system "go", "get", "github.com/Serulian/compiler/cmd/serulian"
-    system "go", "build", "github.com/Serulian/compiler/cmd/serulian"
+    system "go", "get", "github.com/serulian/compiler/cmd/serulian"
+    system "go", "build", "github.com/serulian/compiler/cmd/serulian"
 
     bin.install "serulian"
   end
