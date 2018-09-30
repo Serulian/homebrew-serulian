5b70a82019bc195d723d659d8fbadc0a44caea45
Joseph Schorr <jschorr@gmail.com>
2018-09-29 22:30:23 -0400

    Update to toolkit version v0.3.2

--- serulian.rb
+++ serulian.rb
@@ -2,8 +2,8 @@ class Serulian < Formula
   desc "Serulian toolkit"
   homepage "https://serulian.io"
   head "https://github.com/serulian/compiler.git"
-  url "https://github.com/serulian/compiler/releases/download/v0.3.1/serulian-darwin-amd64", :using => :nounzip
-  version "v0.3.1"
+  url "https://github.com/serulian/compiler/releases/download/v0.3.2/serulian-darwin-amd64", :using => :nounzip
+  version "v0.3.2"
 
   bottle :unneeded
   
